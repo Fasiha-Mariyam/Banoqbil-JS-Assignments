e95002c3c13d43e6e553da30fdd50a4001d9e800
fariha mariyam <farihamariyam183@gmail.com>
2023-04-06 15:30:49 +0500

    FEAT: JS Intro Class

---
+++ scripts/app.js
@@ -0,0 +1,48 @@
+                                  // Day 1 //
+
+// console.log(2+6)
+// console.log(2-6)
+// console.log(2*6)
+// console.log(2/6)
+// console.log(2**6)
+// console.log(9+3)
+
+// alert("Hello user")
+// var user = prompt("Your Name PLease","no")
+// alert(user)
+
+// let arr = ['hi','im','fsi']
+// document.write(arr)
+// console.log(arr)
+
+// var imFromBanoQabil = "jee Sahi kaha , im good"; //camelCase
+// var ImFromBanoQabil; // PascalCase
+// var im_from_bano_qabil //snake_case
+
+// alert(imFromBanoQabil)
+// var num = prompt("write any number")
+// console.log(num)
+
+// let num1 = +prompt("Write Number 1 :")
+// let num2 = +prompt("Write Number 2 :")
+// console.log("Number i:"+(num1) + " Number ii:"+(num2) +" = "+ (num1+num2))
+// console.log(num1+num2)
+// console.log("1"+"6")
+
+// console.log(((1*(2-2))*(8/9)*6)+(2*6)/1)
+// var name = "Im From"
+// var fname = "BanoQabil"
+// console.log(name+ " " + fname);
+
+// var num = 2
+// console.log(num)
+// num = num+10
+// num *=10
+// num++
+// ++num
+// num--
+// console.log(num+"6")
+
+                                   // Day 2 //
+
+
